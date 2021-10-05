0ed67064964c6dca9e3ddffd6258dd3a880a2553
Artin Peyvasteh <aripey11@gmail.com>
2021-10-05 11:16:00 -0700

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Week5.Models;
+
+namespace Week5.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Player> Players{get; set;}
+        public DbSet<Team> Teams {get; set;}
+    }
+}
